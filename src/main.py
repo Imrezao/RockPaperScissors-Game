b50016eaae4b4bd29c4548b19d798190202c2cfa
Reza Omidi <m.r.omidi484@gmail.com>
2026-05-02 15:00:08 +0330

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,60 @@
+"""
+Author: Reza Omidi
+Date Created: 4/19/2025
+Description: RockPaperScissor Game.
+"""
+
+import random
+from typing import List, Tuple          
+
+
+class RockPaperScissors:
+    """
+    Main class for RockPaperScissor Game.
+    """
+    def __init__(self,choices, name) -> None:
+        self.choices: list[str] = choices
+        self.name = name
+        
+    def get_user_choice(self) -> str:
+        while True:
+            user_input: str = input('Enter your choice (rock/paper/scissors): ')
+            if user_input.lower() in self.choices:
+                return user_input.lower()
+            
+            print('Invalid choice, try again')
+            return self.get_user_choice()
+
+    def get_computer_choice(self) -> str:
+        return random.choice(self.choices)
+    
+    def decide_winner(self, user_choice: str, computer_choice: str) -> str:
+        if user_choice == computer_choice :
+            return 'It is a Tie'
+
+        for item in [('rock', 'scissors'), ('paper', 'rock'), ('scissors', 'paper')]:
+            if user_choice == item[0] and computer_choice == item[1]:
+                return 'User wins'
+     
+        return 'Computer wins'    
+
+
+    def play(self) -> None:
+        user_choice: str = self.get_user_choice()
+        computer_choice: str = self.get_computer_choice()
+        winner: str = self.decide_winner(user_choice, computer_choice)
+        print(f"Computer choice: {computer_choice} \nUser choice: {user_choice}")
+        print(winner)
+        
+
+if __name__ == '__main__':
+    game = RockPaperScissors(['rock', 'paper', 'scissors'], 'Reza')
+
+    while True:
+        game.play()
+        print('Do u wanna play again? y/n')
+        if input().lower() == 'y':
+            continue
+
+        print('good bye')
+        break
